40e12428f9b0d56f8259ce5c8b9ad915dd5804f9
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2025-05-31 13:15:30 +0900

    2
    완성한듯

--- main.c
+++ main.c
@@ -54,30 +54,53 @@ void dequeue(hNode* h){
 		exit(1);
 	} 
 	else{
-		qNode* temp = h->front; //삭제할 노드 
+		qNode* temp = h->front; //삭제할 노드  
+		printf("퇴소:%s\n",temp->name); //퇴소 요청 
 		h->front = h->front->next; //맨앞 옮기기
 		free(temp); //메모리 반납
-		h->length--; 
-		printf("퇴소:%s\n",temp->name); 
+		h->length--;
 	}
 }
 
+void qprint(hNode* h){
+	int i;
+	qNode* pp = h->front; //프린트할거 포인터 
+	//pnode[h->length]; //프린트할 배열 정의
+	printf("현재 큐 상태:"); 
+	for(i=0;i<h->length-1;i++){ //끝 노드 전까지 화살표있게 
+		//if(pp->next!=NULL){
+		printf("%s->",pp->name);
+		//pNode[i]= pp->name
+		pp=pp->next;//다음노드 
+		//}
+	}
+	printf("%s\n",pp->name); //마지막 노드 
+	/*for(i=0;i<h->length;i++){
+		printf("현재 큐 상태:%s\n",pp->name);
+	}*/
+	
+}
  
 int main(int argc, char *argv[]) {
 	//피난소 입소 대기열 시스템 구현
 	hNode *waiting; //공백대기열 생성
+	waiting=(hNode*)malloc(sizeof(hNode));
 	waiting->length = 0;
 	waiting->front = waiting->rear = NULL;
 	 
 	//1.일반 피난민 대기열 등록
 	enqueue(waiting,"Alice");
-	printf("현재 큐 상태:") 
+	qprint(waiting); //연산 후 큐상태  출력 
 	enqueue(waiting,"Bob");
+	qprint(waiting); //연산 후 큐상태  출력
 	//2.VIP피난민 등록 
 	enqueue_front(waiting,"Zena");
+	qprint(waiting); //연산 후 큐상태  출력
 	//3.퇴소 먼저 입장하신 분의 퇴소 (FIFO) 
 	dequeue(waiting);
+	qprint(waiting); //연산 후 큐상태  출력
 	enqueue(waiting,"Tom"); 
+	qprint(waiting); //연산 후 큐상태  출력
 	 
 	return 0;
 }
